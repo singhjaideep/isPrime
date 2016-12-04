0a387b489a1f30733e647b96fd0c53d41ad9f262
Jaideep Singh <singhjaideep@users.noreply.github.com>
2016-12-03 18:21:30 -0600

    Siiiiiiiieve Of ERATOSTHENES!

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # isPrime
-Sample flask api to return whether a number is prime or not. This is not optimised.
-## Todo for v0.0.1
-- http://heinrichhartmann.com/2016/04/03/Dijkstra%27s-Prime-Number-Algorithm.html
-- Add tornado support http://flask.pocoo.org/docs/0.10/deploying/wsgi-standalone/#tornado
+Sample flask api to return whether a number is prime or not. Can be optimised infinitely.
+## Future Todos:
 - Add numpy
-- Make a sample frontend app
+- Make frontend app
--- isprime.py
+++ isprime.py
@@ -2,34 +2,35 @@ import time
 from flask import Flask, jsonify, abort
 app = Flask(__name__)
 
-def isprime(input):
+def calcprime(n):
     returndict = {
-        'id': input,
+        'id': n,
         'title': u'API that returns whether the number is prime or not (NOT optimised)',
         'isprime': True,    #Default
         'description': u'Prime Number', #Default
         'time': u'',
     }
-    if input % 2 == 0 :
-        returndict['description'] = u'Divisible by 2'
+    #Using Sieve Of Eratosthenes
+    from math import sqrt
+    num = n+1
+    sqrtn = int(sqrt(num))
+    noprimes = {j for i in xrange(2,sqrtn) for j in xrange(i**2,num,i)}
+    primes = {i for i in xrange(num) if i not in noprimes}
+    if n in primes:
+        returndict['description'] = u'NOT a prime number'
         returndict['isprime'] = False
         return returndict
-    else:
-        for i in xrange(3, input/2, 2):
-            if input % i == 0:
-                returndict['description'] = u'Divisible by %d' % i
-                returndict['isprime'] = False
-                return returndict
     return returndict
 
 @app.route('/api/isprime/<int:input>', methods=['GET'])
 def is_prime(input):
     start_time = time.time()
-    output = isprime(input)
+    output = calcprime(input)
     if len(output) == 0:
         abort(404)
     output['time'] = str(round(time.time() - start_time, 2)) + ' sec'
     return jsonify({'output': output})
 
 if __name__ == '__main__':
-    app.run()
+    isprime(257)
+    app.run(debug=True)
